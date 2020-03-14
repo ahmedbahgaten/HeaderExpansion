8b25942371f91da0ef76aa08b53c76be3b24af29
ahmedbahgaten <ahmedbahgatnabih@gmail.com>
2020-03-14 18:41:00 +0200

    first commit

--- HeaderExpansion/ViewController.swift
+++ HeaderExpansion/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
+    let maxHeaderHeight: CGFloat = 220
+    let minHeaderHeight: CGFloat = 80
+    var previousScrollOffset: CGFloat = 0
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var headerViewHeight: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        let cell = UINib(nibName: "myCellTableViewCell", bundle: nil)
+        tableView.register(cell, forCellReuseIdentifier: "myCell")
+
     }
-
-
 }
+extension ViewController:UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
 
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! myCellTableViewCell
+        return cell
+    }
+}
+extension ViewController {
+    func canAnimateHeader (_ scrollView: UIScrollView) -> Bool {
+        let scrollViewMaxHeight = scrollView.frame.height + self.headerViewHeight.constant - minHeaderHeight
+        return scrollView.contentSize.height > scrollViewMaxHeight
+    }
+    func setScrollPosition() {
+        self.tableView.contentOffset = CGPoint(x:0, y: 0)
+    }
+}
+extension ViewController {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let scrollDiff = (scrollView.contentOffset.y - previousScrollOffset)
+        let isScrollingDown = scrollDiff > 0
+        let isScrollingUp = scrollDiff < 0
+        if canAnimateHeader(scrollView) {
+            var newHeight = headerViewHeight.constant
+            if isScrollingDown {
+                newHeight = max(minHeaderHeight, headerViewHeight.constant - abs(scrollDiff))
+            } else if isScrollingUp {
+                newHeight = min(maxHeaderHeight, headerViewHeight.constant + abs(scrollDiff))
+            }
+            if newHeight != headerViewHeight.constant {
+                headerViewHeight.constant = newHeight
+                setScrollPosition()
+                previousScrollOffset = scrollView.contentOffset.y
+            }
+        }
+    }
+}
---
+++ HeaderExpansion/myCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  myCellTableViewCell.swift
+//  HeaderExpansion
+//
+//  Created by AHMED on 3/14/20.
+//  Copyright © 2020 AHMED. All rights reserved.
+//
+
+import UIKit
+
+class myCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
